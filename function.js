768d4c8b0b7bdaacb8a0ae443cabe0acab11f395
Douglas de Barros <douglasbl18@gmail.com>
2025-09-10 11:45:39 -0300

    Adicionado 'function.js' com exercícios sobre funções.

---
+++ function.js
@@ -0,0 +1,49 @@
+/*
+//função com objetos.
+let meucarro = {
+    make:"Honda",
+    model:"Acord",
+    year:1998
+};
+
+let meucarro2 = {
+    make:"Fiat",
+    model:"Uno",
+    year:2002
+};
+
+let x;
+let y;
+
+x = meucarro.make;
+
+function minhaFuncao(objeto) {
+    objeto.make = "Toyota";
+}
+
+console.log(x);
+
+minhaFuncao(meucarro);
+
+y = meucarro.make;
+
+console.log(x + " " + y);
+
+console.log(y);
+console.log(x);
+
+
+
+
+//função em uma variável.
+let soma = function(a, b) {
+    return a + b;
+};
+
+let resultado = soma(10, 20);
+console.log(resultado);
+*/
+
+
+
+
